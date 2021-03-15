d78adb4c5aec7c6679b20184351ec6edf0cb9549
Lilit Levonayn <lilt.levonyan@picsart.com>
2021-03-15 18:29:07 +0400

    task lesson N2... comment fixes

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -1,27 +1,116 @@
 public class FirstTask {
     //main method
     public static void main(String[] args) {
-      for (int i =1; i<=9; i++){
-          printline(i);
-      }
+        // line 1
+        System.out.print(' ');
+        System.out.print(' ');
+        System.out.print(' ');
+        System.out.print('-');
+        System.out.print(' ');
+        System.out.print(' ');
+        System.out.print(' ');
+
+        // line 2
+        System.out.println(' ');
+        System.out.print(' ');
+        System.out.print(' ');
+        System.out.print('.');
+        System.out.print(' ');
+        System.out.print('.');
+        System.out.print(' ');
+        System.out.print(' ');
+
+        // line 3
+        System.out.println(' ');
+        System.out.print(' ');
+        System.out.print(' ');
+        System.out.print('|');
+        System.out.print('|');
+        System.out.print('|');
+        System.out.print(' ');
+        System.out.print(' ');
+
+        // line 4
+        System.out.println(' ');
+        System.out.print(' ');
+        System.out.print('-');
+        System.out.print('-');
+        System.out.print('-');
+        System.out.print('-');
+        System.out.print('-');
+        System.out.print(' ');
+
+
+        // line 5
+        System.out.println(' ');
+        System.out.print('-');
+        System.out.print('L');
+        System.out.print('i');
+        System.out.print('l');
+        System.out.print('i');
+        System.out.print('t');
+        System.out.print('-');
+
+        // line 6
+        System.out.println(' ');
+        System.out.print(' ');
+        System.out.print('-');
+        System.out.print('-');
+        System.out.print('-');
+        System.out.print('-');
+        System.out.print('-');
+        System.out.print(' ');
+
+        // line 7
+        System.out.println(' ');
+        System.out.print(' ');
+        System.out.print(' ');
+        System.out.print('|');
+        System.out.print('|');
+        System.out.print('|');
+        System.out.print(' ');
+        System.out.print(' ');
+
+        // line 8
+        System.out.println(' ');
+        System.out.print(' ');
+        System.out.print(' ');
+        System.out.print('.');
+        System.out.print(' ');
+        System.out.print('.');
+        System.out.print(' ');
+        System.out.print(' ');
+
+        // line 9
+        System.out.println(' ');
+        System.out.print(' ');
+        System.out.print(' ');
+        System.out.print(' ');
+        System.out.print('-');
+        System.out.print(' ');
+        System.out.print(' ');
+        System.out.print(' ');
+//      for (int i =1; i<=9; i++){
+//          printline(i);
+//      }
 
     }
 //print method
-    private static void printline(int line){
-        String string1 = "-";
-        String string2 = ". .";
-        String string3 = "|||";
-        String string4 = "Lilit";
-        if (line==1 || line==9)
-        System.out.println("   "+string1+"   ");
-        if (line==2 || line==8)
-        System.out.println("  "+string2+"  ");
-        if (line==3 || line==7)
-        System.out.println("  "+string3+"  ");
-        if(line==4 || line==6)
-        System.out.println(" "+string1+string1+string1+string1+string1+" ");
-        if(line==5)
-        System.out.println(string1+string4+string1);
-
-    }
+//    private static void printline(int line){
+//        String string1 = "-";
+//        String string2 = ". .";
+//        String string3 = "|||";
+//        String string4 = "Lilit";
+//        if (line==1 || line==9)
+//        System.out.println("   "+string1+"   ");
+//        if (line==2 || line==8)
+//        System.out.println("  "+string2+"  ");
+//        if (line==3 || line==7)
+//        System.out.println("  "+string3+"  ");
+//        if(line==4 || line==6)
+//        System.out.println(" "+string1+string1+string1+string1+string1+" ");
+//        if(line==5)
+//        System.out.println(string1+string4+string1);
+//
+//    }
 }
