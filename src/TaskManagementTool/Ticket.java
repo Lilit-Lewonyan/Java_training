fab4ab8f59735c68e9550b26048b30600fa361d8
Lilit Levonayn <lilt.levonyan@picsart.com>
2021-03-29 18:54:34 +0400

    inheritance

--- src/TaskManagementTool/Ticket.java
+++ src/TaskManagementTool/Ticket.java
@@ -1,7 +1,14 @@
 package TaskManagementTool;
 
 public class Ticket {
-    int number;
-    String reporter;
-    String title;
+    protected int number;
+    protected User reporter;
+    protected String title;
+
+    public void creat(int number,User reporter,String title){
+        this.number = number;
+        this.reporter = reporter;
+        this.title = title;
+
+    }
 }
