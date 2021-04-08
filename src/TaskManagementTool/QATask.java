85527d1f2b6c0206b8c192f170860ec51836f64e
Lilit Levonayn <lilt.levonyan@picsart.com>
2021-04-08 23:30:03 +0400

    Ticket as an abstract

--- src/TaskManagementTool/QATask.java
+++ src/TaskManagementTool/QATask.java
@@ -1,4 +1,8 @@
 package TaskManagementTool;
 
 public class QATask extends Ticket {
+    @Override
+    public void creat(int number, User reporter, String title) {
+
+    }
 }
--- src/TaskManagementTool/Task.java
+++ src/TaskManagementTool/Task.java
@@ -1,4 +1,8 @@
 package TaskManagementTool;
 
 public class Task extends Ticket{
+    @Override
+    public void creat(int number, User reporter, String title) {
+
+    }
 }
